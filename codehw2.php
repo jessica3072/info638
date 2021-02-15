e0f6f46d1a1f6493e56809f01072216111fca016
jessica3072 <jessica.3072@gmail.com>
2021-02-14 22:04:00 -0500

    Create codehw2.php
    Updating code to reflect new loop, adding spaces in front of words

---
+++ codehw2.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+ <title>Code Homework #2: Functions</title>
+    </head>
+<body>
+<p>
+    
+    <h2>Challenge: ISBN Validation</h2>
+    
+    
+<?php
+    /*Here we go, attempting the first challenge, trying to remember what function to use here...*/
+    
+?>
+    
+    
+    
+    
+    
+    
+    <p><br>
+    <h2>Challenge: Coin Toss</h2>
+    
+<?php
+    //Coin toss challenge
+
+?>
+    
+    
+    
+    
+    
+</body>
+</html>
