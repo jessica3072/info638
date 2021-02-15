5435c7b8f59cf81b7344b22ad7a3f49d9bebc52e
jessica3072 <jessica.3072@gmail.com>
2021-02-14 23:51:26 -0500

    Update codehw2.php

--- codehw2.php
+++ codehw2.php
@@ -12,8 +12,12 @@
     
     
 <?php
-    /*Here we go, attempting the first challenge, trying to remember what function to use here...*/
-    
+    /*Here we go, attempting the first challenge, trying to remember what function to use here... first obtain the sum of (10 * the first digit, 9 times the second, and so on until 1 times the last digit. Second, divide the sum by 11. If sum leaves no remainder, ISBN = valid.*/
+
+    print "Checking ISBN for validity..." . "<br><br>";
+    print "This is a valid ISBN!";
+    print "This is not a valid ISBN.";
+      
 ?>
     
     
