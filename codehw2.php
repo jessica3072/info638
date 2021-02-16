65d07b6ae1b8bf30650d0c60715ba9339c88aac8
jessica3072 <jessica.3072@gmail.com>
2021-02-16 01:45:38 -0500

    Update codehw2.php

--- codehw2.php
+++ codehw2.php
@@ -8,14 +8,9 @@
     
     <h2>Challenge: ISBN Validation</h2>
 <?php
-    /*first obtain the sum of (10 * the first digit, 9 times the second, and so on until 1 times the last digit. Second, divide the sum by 11. If sum leaves no remainder, ISBN = valid.*/
 
-    
-$myISBN = "0812995821";
-print "Checking ISBN: ". $myISBN . "for validity..." . "<br><br>";
-    
-/* 0812995821 -- 
-    
+/*first obtain the sum of (10 * the first digit, 9 times the second, and so on until 1 times the last digit. Second, divide the sum by 11. If sum leaves no remainder, ISBN = valid.
+0812995821 -- 
 $D1 = 0;
 $D2 = 8;
 $D3 = 1;
@@ -28,47 +23,51 @@ $D9 = 2;
 $D10 = 1;
     
 function ValidateISBN() ?? 
-  
 $SumOfDigits = (10 * $D1) + (9 * $D2) + (8 * $D3) + (7 * $D4) + (6 * $D5) + (5 * $D6) + (4 * $D7) + (3 * $D8) + (2 * $D9) + (1 * $D10);
 $Result = $SumOfDigits / 11;
 
 wait actually maybe substring function ?!
 
-  
-
-$SumOfDigits = (substr($myISBN, -9, 1) * 10) + (substr($myISBN, -8, 1) * 9) + (substr($myISBN, -7, 1) * 8) + (substr($myISBN, -6, 1) * 7) + (substr($myISBN, -5, 1) * 6) + (substr($myISBN, -4, 1) * 5) + (substr($myISBN, -3, 1) * 4) + (substr($myISBN, -2, 1) * 3) + (substr($myISBN, -1, 1) * 2) + (substr($myISBN, 10) * 1);
-    
+$SumOfDigits = (substr($myISBN, -9, 1) * 10) + (substr($myISBN, -8, 1) * 9) + (substr($myISBN, -7, 1) * 8) + (substr($myISBN, -6, 1) * 7) + (substr($myISBN, -5, 1) * 6) + (substr($myISBN, -4, 1) * 5) + (substr($myISBN, -3, 1) * 4) + (substr($myISBN, -2, 1) * 3) + (substr($myISBN, -1, 1) * 2) + (substr($myISBN, 10) * 1); 
 */    
 
-$str = $myISBN;
+
+$myISBN = "0812995821";
+print "Your isbn: " . $myISBN . " is being validated...";
+// everything up to here seems to be ok so far
+//try substr here     
     
-$D1 = $str[0] * 10;
-$D2 = $str[1] * 9;
-$D3 = $str[2] * 8;
-$D4 = $str[3] * 7;
-$D5 = $str[4] * 6;
-$D6 = $str[5] * 5;
-$D7 = $str[6] * 4;
-$D8 = $str[7] * 3;
-$D9 = $str[8] * 2;
-$D10 = $str[9] * 1;
+$D1 = ($myISBN[0]) * 10;
+$D2 = ($myISBN[1]) * 9;
+$D3 = ($myISBN[2]) * 8;
+$D4 = ($myISBN[3]) * 7;
+$D5 = ($myISBN[4]) * 6;
+$D6 = ($myISBN[5]) * 5;
+$D7 = ($myISBN[6]) * 4;
+$D8 = ($myISBN[7]) * 3;
+$D9 = ($myISBN[8]) * 2;
+$D10 = ($myISBN[9]) * 1;
     
-$SumOfDigits = ($D1 + $D2 + $D3 + $D4 + $D5 + $D6 + $D7 + $D8 + $D9 + $D10) ;
-$Result = $SumOfDigits / 11;
-    
-    
+
+function SumOfDigits($ISBNvalidator)
+{
+    $ISBNValidator = ($D1 + $D2 + $D3 + $D4 + $D5 + $D6 + $D7 + $D8 + $D9 + $D10);
 return $Result;
 
-    // $ReturnValue = ValidateISBN 
+// if the number is divided by 11 and the remainder is zero
     
-if ($Result != 0)
+    
+    
+if (SumOfDigits % 11 != 0) 
 {
-    echo "This is not a valid ISBN!" . "<br>";
+    echo "This is a not valid ISBN" . "<br>";
 }
 else    
 {
     echo "This is a valid ISBN!";
-    };
+    }
+}
+    
     
 // getting closer! still working out kinks
 /* if ($some_variable % $some_other_variable != 0) 
@@ -76,17 +75,8 @@ else
 else {
 echo "isbn is valid!" 
 }
-*/
-
-
-
-
-      
+*/      
 ?>
-    
-    
-    
-    
     
     
     <p><br>
