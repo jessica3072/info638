9f401daf916eae4f7f11b3286e72b7e01ceaf216
jessica3072 <jessica.3072@gmail.com>
2021-02-23 20:03:35 -0500

    Update codehw3.php

--- codehw3.php
+++ codehw3.php
@@ -6,9 +6,23 @@
 </head>
 <html>
 <body>
+    
+    
 <?php
+echo "<h3>" . "Code Homework #3 [Arrays]" . "</h3>";
+echo "<h4>" . "1. Challenge: Book lists" . "</h4>";
+/*getting very comfortable now interpolating html within php after practicing taking notes in it over the past week! */
 
+//multi-dimensional arrays
+//table
+  
+echo "<table><tr><th>" . "Title" . "</th><th>" . "First Name" . "</th><th>" . "Last Name" . "</th><th>" . "Number of Pages" . "</th><th>" . "Type" . "</th><th>" . "Price" . "</th></tr><tr><td>" . "PHP and MySQL Web Development" . "</td><td>" . "Luke" . "</td><td>" . "Welling" . "</td></tr>" . "<tr><td>" . "Web Design with HTML, CSS, JavaScript and jQuery" . "</td><td>" . "Jon" . "</td><td>" . "Duckett" . "</td></tr</table><br><br>";
+    
 
+    
+//part two
+echo "<h4>" . "2. Challenge: Coin Toss, continued" . "</h4>";
+    
 
 ?>
 </body>
