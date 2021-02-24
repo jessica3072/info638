22366bba20805add39eb1760746506abbcf66796
jessica3072 <jessica.3072@gmail.com>
2021-02-23 23:08:04 -0500

    more editing and fussing

--- codehw3.php
+++ codehw3.php
@@ -35,7 +35,7 @@ $booklistdata = array("JavaScript and JQuery: Interatcive Front-End Web Developm
 //i'm sure there's a way to do this nesting arrays ???    
 
 echo "<i>" . "Feel free to hover over the data :)" . "</i><br><br>";
-echo "<table><caption>" . "Placeholder Caption for Lovely Stylized Table" . "</caption><thead><tr><th>" . "Title" . "</th><th>" . "First Name" . "</th><th>" . "Last Name" . "</th><th>" . "Number of Pages" . "</th><th>" . "Type" . "</th><th>" . "Price" . "</th></tr></thead><tbody><tr><td>" . "PHP and MySQL Web Development" . "</td><td>" . "Luke" . "</td><td>" . "Welling" . "</td><td>" . "144" . "</td><td>" . "Paperback" . "</td><td>" . "$31.63" . "</td></tr><tr><td>" . "Web Design with HTML, CSS, JavaScript and jQuery" . "</td><td>" . "Jon" . "</td><td>" . "Duckett" . "</td><td>" . "135" . "</td><td>" . "Paperback" . "</td><td>" . "$41.23" . "</td></tr><tr><td>" . "$variable1" . "</td><td>" . "$variable2" . "</td><td>" . "$variable3" . "</td><td>" . "$variable4" . "</td><td>" . "$variable5" . "</td><td>" . "$variable6" . "</td></tr><tr><td>" . "$booklistdata[0]" . "</td><td>" . "$booklistdata[1]" . "</td><td>" . "$booklistdata[2]" . "</td><td>" . "$booklistdata[3]" . "</td><td>" . "$booklistdata[4]" . "</td><td>" . "$booklistdata[5]" . "</td><tr><td>" . "$booklistdata[6]" . "</td><td>" . "$booklistdata[7]" . "</td><td>" . "$booklistdata[8]" . "</td><td>" . "$booklistdata[9]" . "</td><td>" . "$booklistdata[10]" . "</td><td>" . "$booklistdata[11]" . "</td><tr><td>" . "$booklistdata[12]" . "</td><td>" . "$booklistdata[13]" . "</td><td>" . "$booklistdata[14]" . "</td><td>" . "$booklistdata[15]" . "</td><td>" . "$booklistdata[16]" . "</td><td>" . "$booklistdata[17]" . "</td></tr></tbody><tfoot><tr>" . "</tr></tfoot></table>";
+echo "<table><caption>" . "Lovely Stylized Table" . "</caption><thead><tr><th>" . "Title" . "</th><th>" . "First Name" . "</th><th>" . "Last Name" . "</th><th>" . "Number of Pages" . "</th><th>" . "Type" . "</th><th>" . "Price" . "</th></tr></thead><tbody><tr><td>" . "PHP and MySQL Web Development" . "</td><td>" . "Luke" . "</td><td>" . "Welling" . "</td><td>" . "144" . "</td><td>" . "Paperback" . "</td><td>" . "$31.63" . "</td></tr><tr><td>" . "Web Design with HTML, CSS, JavaScript and jQuery" . "</td><td>" . "Jon" . "</td><td>" . "Duckett" . "</td><td>" . "135" . "</td><td>" . "Paperback" . "</td><td>" . "$41.23" . "</td></tr><tr><td>" . "$variable1" . "</td><td>" . "$variable2" . "</td><td>" . "$variable3" . "</td><td>" . "$variable4" . "</td><td>" . "$variable5" . "</td><td>" . "$variable6" . "</td></tr><tr><td>" . "$booklistdata[0]" . "</td><td>" . "$booklistdata[1]" . "</td><td>" . "$booklistdata[2]" . "</td><td>" . "$booklistdata[3]" . "</td><td>" . "$booklistdata[4]" . "</td><td>" . "$booklistdata[5]" . "</td><tr><td>" . "$booklistdata[6]" . "</td><td>" . "$booklistdata[7]" . "</td><td>" . "$booklistdata[8]" . "</td><td>" . "$booklistdata[9]" . "</td><td>" . "$booklistdata[10]" . "</td><td>" . "$booklistdata[11]" . "</td><tr><td>" . "$booklistdata[12]" . "</td><td>" . "$booklistdata[13]" . "</td><td>" . "$booklistdata[14]" . "</td><td>" . "$booklistdata[15]" . "</td><td>" . "$booklistdata[16]" . "</td><td>" . "$booklistdata[17]" . "</td></tr></tbody><tfoot><tr>" . "</tr></tfoot></table>";
     
 
 
@@ -46,7 +46,10 @@ echo "<table><caption>" . "Placeholder Caption for Lovely Stylized Table" . "</c
 //part two
 echo "<br><h4>" . "2. Challenge: Coin Toss, continued" . "</h4>";
 //modify program to 1: be a function that takes one argument
+
     
+/*2: modify code to use this argument to allow a variable number of heads in a row to be chosen
+*/
 //whoops running out of time
 
     
