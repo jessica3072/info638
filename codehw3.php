ac808ab4d1356cd8ed439a06beb14c9f1bca8fc7
jessica3072 <jessica.3072@gmail.com>
2021-02-23 20:42:02 -0500

    adding data to table

--- codehw3.php
+++ codehw3.php
@@ -17,7 +17,7 @@ echo "<h4>" . "1. Challenge: Book lists" . "</h4>";
 //multi-dimensional arrays
 //table
   
-echo "<table><caption>" . "Lovely Table with Book List Data" . "</caption><thead><tr><th>" . "Title" . "</th><th>" . "First Name" . "</th><th>" . "Last Name" . "</th><th>" . "Number of Pages" . "</th><th>" . "Type" . "</th><th>" . "Price" . "</th></tr></thead><tbody><tr><td>" . "PHP and MySQL Web Development" . "</td><td>" . "Luke" . "</td><td>" . "Welling" . "</td></tr>" . "<tr><td>" . "Web Design with HTML, CSS, JavaScript and jQuery" . "</td><td>" . "Jon" . "</td><td>" . "Duckett" . "</td></tr></tbody><tfoot><tr>" . "</tr></tfoot></table>";
+echo "<table><caption>" . "Lovely Table with Book List Data" . "</caption><thead><tr><th>" . "Title" . "</th><th>" . "First Name" . "</th><th>" . "Last Name" . "</th><th>" . "Number of Pages" . "</th><th>" . "Type" . "</th><th>" . "Price" . "</th></tr></thead><tbody><tr><td>" . "PHP and MySQL Web Development" . "</td><td>" . "Luke" . "</td><td>" . "Welling" . "</td><td>" . "144" . "</td></tr>" . "<tr><td>" . "Web Design with HTML, CSS, JavaScript and jQuery" . "</td><td>" . "Jon" . "</td><td>" . "Duckett" . "</td><td>" . "135" . "</td></tr></tbody><tfoot><tr>" . "</tr></tfoot></table>";
     
 
     
