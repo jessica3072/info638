030d280e0a8736c1fa288fe1ef9edd9a45815f42
jessica3072 <jessica.3072@gmail.com>
2021-02-23 20:38:47 -0500

    k

--- codehw3.php
+++ codehw3.php
@@ -3,6 +3,7 @@
 <head>
     <title>Code HW 3
     </title>
+    <link href="style.css" rel="stylesheet" type="text/css">
 </head>
 <html>
 <body>
@@ -16,14 +17,22 @@ echo "<h4>" . "1. Challenge: Book lists" . "</h4>";
 //multi-dimensional arrays
 //table
   
-echo "<table><tr><th>" . "Title" . "</th><th>" . "First Name" . "</th><th>" . "Last Name" . "</th><th>" . "Number of Pages" . "</th><th>" . "Type" . "</th><th>" . "Price" . "</th></tr><tr><td>" . "PHP and MySQL Web Development" . "</td><td>" . "Luke" . "</td><td>" . "Welling" . "</td></tr>" . "<tr><td>" . "Web Design with HTML, CSS, JavaScript and jQuery" . "</td><td>" . "Jon" . "</td><td>" . "Duckett" . "</td></tr</table><br><br>";
+echo "<table><caption>" . "Lovely Table with Book List Data" . "</caption><thead><tr><th>" . "Title" . "</th><th>" . "First Name" . "</th><th>" . "Last Name" . "</th><th>" . "Number of Pages" . "</th><th>" . "Type" . "</th><th>" . "Price" . "</th></tr></thead><tbody><tr><td>" . "PHP and MySQL Web Development" . "</td><td>" . "Luke" . "</td><td>" . "Welling" . "</td></tr>" . "<tr><td>" . "Web Design with HTML, CSS, JavaScript and jQuery" . "</td><td>" . "Jon" . "</td><td>" . "Duckett" . "</td></tr></tbody><tfoot><tr>" . "</tr></tfoot></table>";
     
 
     
 //part two
-echo "<h4>" . "2. Challenge: Coin Toss, continued" . "</h4>";
+echo "<br><h4>" . "2. Challenge: Coin Toss, continued" . "</h4>";
+//why is this showing up before the table....
+//update ok it's fixed now?
     
 
+
+    
+
+
+    
+    
 ?>
 </body>
 </html>
